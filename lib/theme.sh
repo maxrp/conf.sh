33a7dea5f63244147c3e745e83ac457988d716d4
Max R.D. Parmer <maxp@pdx.edu>
2013-12-01 03:23:13 +0000

    Add wmii/xorg handling.

---
+++ lib/theme.sh
@@ -0,0 +1,7 @@
+# Xorg
+install -m 0700 $SRCDIR/theme/xinitrc      $HOME/.xinitrc
+install -m 0600 $SRCDIR/theme/Xresources   $HOME/.Xresources
+
+# WMII
+mkdir -p .wmii
+install -m 0700 $SRCDIR/theme/wmiirc_local $HOME/.wmii/wmiirc_local
