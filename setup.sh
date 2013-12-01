9df662c3a6650382399c5c47e004b45d73c019a0
Max R.D. Parmer <maxp@trystero.is>
2013-12-01 14:58:37 -0800

    Start handling options and add help text.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/bash
 
+### Usage: bash setup.sh [options] module,...
+###
+###       -i            Install all listed modules
+###       -l            List available modules
+
+help=$(grep "^### " "$0" | cut -c 5-)
+
 . ./lib/config.sh
 
-MODULES+=(bash)
-MODULES+=(dircolors)
-MODULES+=(gpg)
-MODULES+=(proxychains)
-MODULES+=(ssh)
-MODULES+=(theme)
-MODULES+=(tmux)
-MODULES+=(vim)
-
+while getopts ":hli:" opt; do
+  case $opt in
+    h)
+      echo "${help}"
+      ;;
+    i)
+      
+      install_modules $OPTARG
+      ;;
+    \?)
+      warn "Invalid option: -${OPTARG}"
+      warn "${help}"
+      ;;
+     *)
+      warn "${help}"
+      ;;
+  esac
+done
 
 # ensure external sources are up-to-date
 update_submodules > /dev/null
