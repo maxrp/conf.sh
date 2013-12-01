eefd8858682df6ca228bb6fed9b3ec8971a15928
Max R.D. Parmer <maxp@trystero.is>
2013-12-01 15:05:14 -0800

    Move git submodule updating into own opt.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ### Usage: bash setup.sh [options] module,...
 ###
+###       -U            Update all git submodules
 ###       -i            Install all listed modules
 ###       -l            List available modules
 
@@ -9,8 +10,12 @@ help=$(grep "^### " "$0" | cut -c 5-)
 
 . ./lib/config.sh
 
-while getopts ":hli:" opt; do
+while getopts ":Uhli:" opt; do
   case $opt in
+    U)
+      # ensure external sources are up-to-date
+      update_submodules
+      ;;
     h)
       echo "${help}"
       ;;
@@ -27,7 +32,3 @@ while getopts ":hli:" opt; do
       ;;
   esac
 done
-
-# ensure external sources are up-to-date
-update_submodules > /dev/null
-
