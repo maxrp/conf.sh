2641b364afdfd33b18af07d7cc829fbdb3c7dffe
Max R.D. Parmer <maxp@pdx.edu>
2013-12-02 05:47:49 +0000

    Fixes some style issues

--- setup.sh
+++ setup.sh
@@ -22,24 +22,35 @@ MODBASE=$(readlink -f ${PWD}/modules)
 HELP=$(grep "^###" "$0" | cut -c 5-)
 
 # Shorthand for stderr
-function warn(){
+warn(){
   echo "$*" > /dev/fd/2
 }
 
 # The listing for a module looks like:
 #   ## <name>: description 
-function list_modules(){
+list_modules(){
     grep -h '^## ' $MODBASE/*.sh | cut -c 4-
 }
 
 # Run git and get that ...
-function update_submodules(){
+update_submodules(){
   git submodule init
   git submodule update --recursive
   git submodule foreach git pull origin master
 }
 
-function install_modules(){
+config_install(){
+  source="${SRCDIR}/${1}"
+  dest="${HOME}/.${2}"
+  if [ -d $source ]; then
+    cmd='cp -R'
+  else
+    cmd='install -D -m 0600'
+  fi
+  $cmd $source $dest
+}
+
+install_modules(){
     if [[ -z $1 ]]; then
         warn 'A comma-separated list of arguments is required.'
     else
@@ -51,7 +62,7 @@ function install_modules(){
       mod_path="${MODBASE}/${module}.sh"
       if [[ -f $mod_path ]]; then
         echo " + Running module: ${module}"
-        #. "${mod_path}"
+        . "${mod_path}"
       else
         warn " - Module '${module}' doesn't exist."
       fi
@@ -88,16 +99,3 @@ while getopts "Uhli:" opt; do
   esac
 done
 # }}}
-
-function config_install(){
-  source="${SRCDIR}/${1}"
-  dest="${HOME}/.${1}"
-  if [[ -n $2 ]]; then
-    source="${source}/${2}"
-    dest="${HOME}/.${2}"
-  fi
-  if [[ -z $installopts ]]; then
-    installopts='-C --mode=0600'
-  fi 
-  install $installopts $source $dest
-}
