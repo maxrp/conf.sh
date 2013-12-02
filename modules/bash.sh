3e9ffac9d32df7da1556cf6e8adf7ff4632e8677
Max R.D. Parmer <maxp@pdx.edu>
2013-12-02 06:46:14 +0000

    Update for new config_install()

--- modules/bash.sh
+++ modules/bash.sh
@@ -1,5 +1,5 @@
 ## bash: bashrc etc.
 FILES=(bashrc bash_prompt bash_aliases)
 for file in "${FILES[@]}"; do
-  config_install bash $file
+  config_install bash/$file $file
 done
--- modules/vim.sh
+++ modules/vim.sh
@@ -1,5 +1,5 @@
 ## vim: vimrc, pathogen & vim bundles
-config_install vim vimrc
-mkdir -p ${HOME}/.vim/autoload
-cp -R $SRCDIR/vim/autoload/pathogen/autoload/pathogen.vim $HOME/.vim/autoload
-cp -R $SRCDIR/vim/bundle $HOME/.vim 2>&1 | grep -v '.git'
+config_install vim/vimrc vimrc
+config_install vim/autoload/pathogen/autoload/pathogen.vim vim/autoload/pathogen.vim
+# TODO: fix this by fixing config_install()
+config_install vim/bundle vim/bundle
