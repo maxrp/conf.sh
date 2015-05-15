38c1fb5bb01f58aafa65f75deda73eaf8617642d
Maximus R.D. Parmer <maxp@pdx.edu>
2015-05-15 14:37:37 -0700

    Improves documentation somewhat

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ least) cross-platform support was iffy or inconsistent.
 So I wrote this. It's intended to work anywhere you have have a POSIX shell,
 and to work really well anywhere you have a POSIX shell and git.
 
+## Requirements
+Older systems may only provide `command -v` as part of User Portability
+Utilities as it was flagged in [The Open Group Base Specifications Issue 6/
+POSIX.1-2004][1], by [The Open Group Base Specifications Issue 7/POSIX.1-2008][2]
+it was standard. `checkbashisms` will complain about this.
+
 ## Usage
 ```
 Usage: sh conf.sh [-Uvulands] [-m "module module ..."]
@@ -44,3 +50,7 @@ Examples:
  - Optional self-installation to $PREFIX
  - Roll up self and configs into a single archive for easy transport
  - Test suite for: ash, bash, dash, pdksh, mksh, yash
+
+## References
+[1]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/command.html "man page for `command`, IEEE Std 1003.1, 2004 Edition"
+[2]: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/command.html "man page for `command`, IEEE Std 1003.1, 2013 Edition"
--- conf.sh
+++ conf.sh
@@ -8,7 +8,13 @@
 ### 
 ### So I wrote this. It's intended to work anywhere you have have a POSIX shell,
 ### and to work really well anywhere you have a POSIX shell and git.
-### 
+###
+###### Requirements
+### Older systems may only provide `command -v` as part of User Portability
+### Utilities as it was flagged in [The Open Group Base Specifications Issue 6/
+### POSIX.1-2004][1], by [The Open Group Base Specifications Issue 7/POSIX.1-2008][2]
+### it was standard. `checkbashisms` will complain about this.
+###
 ###### Usage
 ### ```
 ###~Usage: sh conf.sh [-Uvulands] [-m "module module ..."]
@@ -45,6 +51,10 @@
 ###  - Optional self-installation to $PREFIX
 ###  - Roll up self and configs into a single archive for easy transport
 ###  - Test suite for: ash, bash, dash, pdksh, mksh, yash
+###
+###### References
+### [1]: http://pubs.opengroup.org/onlinepubs/009695399/utilities/command.html "man page for `command`, IEEE Std 1003.1, 2004 Edition"
+### [2]: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/command.html "man page for `command`, IEEE Std 1003.1, 2013 Edition"
 
 # Global variables for the script and it's modules {{{
 # Why weird assignment? To ensure trickery isn't done via newlines in dirname.
