ab9952f770a1792679e2a8df09eda81a5f15c5cb
Max R.D. Parmer <maxp@trystero.is>
2015-05-18 22:14:17 -0700

    Swap refsh back, add busybox sh.

--- tests.sh
+++ tests.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 HOME="$(mktemp -d)"       # this shouldn't write anything, but just in case
-REF_SH='bash'
+REF_SH='dash'
 TEST_OPTS="-lna"
+
+alias busyboxsh="busybox sh"
 
 main() {
     reflog="${HOME}/ref.log"
     ${REF_SH} conf.sh ${TEST_OPTS} 2>/dev/null > "${reflog}"
-    for shell in bash dash ksh mksh yash; do
-        if command -v ${shell} > /dev/null; then
+    for shell in bash busyboxsh dash ksh mksh yash; do
+        if command -v "${shell}" > /dev/null; then
             echo "** Testing ${shell}..."
             shlog="${HOME}/${shell}.log"
             ${shell} conf.sh ${TEST_OPTS} 2>/dev/null > "${shlog}"
