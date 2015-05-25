668f3b705c00c08a7fe9e84e314fa8c6c19eaf01
Max R.D. Parmer <maxp@trystero.is>
2015-05-25 11:27:36 -0700

    Adds shell version information

--- tests.sh
+++ tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 HOME="$(mktemp -d)"       # this shouldn't write anything, but just in case
+SELF=$(readlink -f "${0}")
+TESTLIBDIR=$(dirname "${SELF}")/test_lib
 REFERENCE_SH='dash'
 TEST_OPTS="-lna"
 export LC_ALL="POSIX"
@@ -8,6 +10,9 @@ export LC_ALL="POSIX"
 if command -v busybox > /dev/null; then
     busybox_sh(){
         busybox sh "${@}"
+    }
+    busybox_sh_version() {
+        busybox | head -1 | awk '{ print $1, $2;}'
     }
 fi;
 
@@ -17,20 +22,40 @@ if command -v bash > /dev/null; then
     }
 fi;
 
+bash_version() {
+    bash "${TESTLIBDIR}/version.bash"
+}
+ksh_version() {
+    ksh "${TESTLIBDIR}/version.ksh"
+}
+mksh_version() {
+    mksh "${TESTLIBDIR}/version.ksh"
+}
+posix_compliant_bash_version() {
+    bash --posix "${TESTLIBDIR}/version.bash"
+}
+yash_version(){
+    yash "${TESTLIBDIR}/version.yash"
+}
 
 main() {
     reflog="${HOME}/ref.log"
     ${REFERENCE_SH} conf.sh ${TEST_OPTS} 2>/dev/null > "${reflog}"
     for shell in bash busybox_sh dash ksh mksh posix_compliant_bash yash; do
         if command -v "${shell}" > /dev/null; then
+            shell_version="${shell}_version"
+            if command -v "${shell_version}" > /dev/null ; then
+                shellver=$("${shell}_version")
+            fi
             echo "** Testing ${shell}..."
             shlog="${HOME}/${shell}.log"
             ${shell} conf.sh ${TEST_OPTS} 2>/dev/null > "${shlog}"
             if diff -q "${shlog}" "${reflog}"; then
-                echo "Passed: ${shell}"
+                echo "Passed: ${shell} ${shellver}"
             else
                 echo "Failed: ${shell}"
             fi
+            shellver=""
         else
             echo "** Skipped: ${shell} (not found)."
         fi;
